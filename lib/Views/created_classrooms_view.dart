5e679dbe75342851e3d215d2fd74c7ed8680c904
Sakib Hassan <skbhssn.sh@gmail.com>
2023-04-11 20:58:17 +0600

    Classrooms listview updated.

--- lib/Views/created_classrooms_view.dart
+++ lib/Views/created_classrooms_view.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:grader_io/Controllers/joined_classrooms_view_controller.dart';
@@ -13,6 +16,7 @@ class CreatedClassroomsView extends ConsumerStatefulWidget {
 }
 
 class CreatedClassroomsViewState extends ConsumerState<CreatedClassroomsView> {
+
   late AsyncValue<CreatedClassrooms> createdClassrooms;
 
   @override
@@ -26,41 +30,104 @@ class CreatedClassroomsViewState extends ConsumerState<CreatedClassroomsView> {
   @override
   Widget build(BuildContext context) {
     createdClassrooms = ref.watch(createdClassroomsViewControllerProvider);
+    double width = MediaQuery. of(context). size. width;
+    double height = MediaQuery. of(context). size. height;
 
     return createdClassrooms.when(
-      data: (createdClassrooms) => Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: RefreshIndicator(
-          onRefresh: () async {
-            ref
-                .read(joinedClassroomsViewControllerProvider.notifier)
-                .fetchJoinedClassrooms();
-          },
-          child: createdClassrooms.classrooms!.isEmpty
-              ? const Center(child: Text('No Classrooms created'))
-              : ListView.builder(
-                  itemCount: createdClassrooms.classrooms!.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Material(
-                        elevation: 4.0,
-                        shadowColor: Colors.blueGrey,
-                        child: ListTile(
-                          onTap: () => {
-                            GoRouter.of(context).push('/summary_of_assignments_in_created_classroom/${createdClassrooms.classrooms![index].name!}/${createdClassrooms.classrooms![index].code!}',)
-                          },
-                          title:
-                              Text(createdClassrooms.classrooms![index].name!),
-                          subtitle: Text(createdClassrooms
-                              .classrooms![index].description!),
+      data: (createdClassrooms) => SingleChildScrollView(
+        child: Container(
+          height: height,
+          width: width,
+          child: GridView.count(
+            primary: false,
+            padding: const EdgeInsets.all(20),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: width ~/ 350,
+            childAspectRatio: 1.2,
+            children: <Widget>[
+                for (var item in createdClassrooms.classrooms!) 
+                  InkWell(
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      child: Card(
+                        color: Colors.blueGrey,
+                        elevation: 10.0,
+                        shadowColor: Colors.black,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min, // add this
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              flex: 3, 
+                              child: Container(
+                                padding: EdgeInsets.all(15),
+                                child: Text(
+                                  item.name!,
+                                  maxLines: 2,
+                                  style: TextStyle(
+                                    overflow: TextOverflow.ellipsis,
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              )
+                            ),
+                            Expanded(
+                              flex: 4, 
+                              child: Container(
+                                padding: EdgeInsets.all(15),
+                                width: double.infinity,
+                                color: Colors.white,
+                                
+                                  child: Text(
+                                    item.description!,
+                                    maxLines: 4,
+                                    style: TextStyle(
+                                      overflow: TextOverflow.ellipsis,
+                                      color: Colors.black,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                
+                              )
+                            ),
+                            Expanded(
+                              flex: 2, 
+                              child: Container(
+                                color: Colors.white,
+                                child: Row(
+                                  children: [
+                                    Expanded(child: SizedBox(),),
+                                    Container(
+                                      padding: EdgeInsets.all(5),
+                                      child: Tooltip(
+                                        message: "Copy classroom code",
+                                        child: IconButton(
+                                          icon: Icon(Icons.content_copy),
+                                          onPressed:() {
+                                            Clipboard.setData(ClipboardData(text: item.code!));
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                  ]
+                                ),
+                              )
+                            ),
+                          ],
                         ),
                       ),
-                    );
-                  },
-                ),
+
+                    ),
+                    onTap: () => {
+                      GoRouter.of(context).push('/summary_of_assignments_in_created_classroom/${item.name!}/${item.code!}',)
+                    },
+                  )
+              ],
+            )
+          ),
         ),
-      ),
       error: (e, s) => Center(
         child: Text(e.toString()),
       ),
--- lib/Views/joined_classrooms_view.dart
+++ lib/Views/joined_classrooms_view.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:grader_io/Controllers/joined_classrooms_view_controller.dart';
@@ -30,37 +33,103 @@ class JoinedClassroomsViewState extends ConsumerState<JoinedClassroomsView> {
   Widget build(BuildContext context) {
 
     joinedClassrooms = ref.watch(joinedClassroomsViewControllerProvider);
+    double width = MediaQuery. of(context). size. width;
+    double height = MediaQuery. of(context). size. height;
 
     return joinedClassrooms.when(
-      data: (joinedClassrooms) => Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: RefreshIndicator(
-          onRefresh: ()async{
-            ref.read(joinedClassroomsViewControllerProvider.notifier).fetchJoinedClassrooms();
-          },
-          child: joinedClassrooms.classrooms!.isEmpty?
-          const Center(child: Text('No Classrooms joined')):
-          ListView.builder(
-            itemCount: joinedClassrooms.classrooms!.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Material(
-                  elevation: 4.0,
-                  shadowColor: Colors.blueGrey,
-                  child: ListTile(
-                    onTap: () => {
-                      GoRouter.of(context).push('/summary_of_assignments_in_joined_classroom/${joinedClassrooms.classrooms![index].name!}/${joinedClassrooms.classrooms![index].code!}',)
+      data: (joinedClassrooms) => SingleChildScrollView(
+        child: Container(
+          height: height,
+          width: width,
+          child: GridView.count(
+            primary: false,
+            padding: const EdgeInsets.all(20),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: width ~/ 350,
+            childAspectRatio: 1.2,
+            children: <Widget>[
+                for (var item in joinedClassrooms.classrooms!) 
+                  InkWell(
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      child: Card(
+                        color: Colors.blueGrey,
+                        elevation: 10.0,
+                        shadowColor: Colors.black,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min, // add this
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              flex: 3, 
+                              child: Container(
+                                padding: EdgeInsets.all(15),
+                                child: Text(
+                                  item.name!,
+                                  maxLines: 2,
+                                  style: TextStyle(
+                                    overflow: TextOverflow.ellipsis,
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              )
+                            ),
+                            Expanded(
+                              flex: 4, 
+                              child: Container(
+                                padding: EdgeInsets.all(15),
+                                width: double.infinity,
+                                color: Colors.white,
+                                
+                                  child: Text(
+                                    item.description!,
+                                    maxLines: 4,
+                                    style: TextStyle(
+                                      overflow: TextOverflow.ellipsis,
+                                      color: Colors.black,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                
+                              )
+                            ),
+                            Expanded(
+                              flex: 2, 
+                              child: Container(
+                                color: Colors.white,
+                                child: Row(
+                                  children: [
+                                    Expanded(child: SizedBox(),),
+                                    Container(
+                                      padding: EdgeInsets.all(5),
+                                      child: Tooltip(
+                                        message: "Copy classroom code",
+                                        child: IconButton(
+                                          icon: Icon(Icons.content_copy),
+                                          onPressed:() {
+                                            Clipboard.setData(ClipboardData(text: item.code!));
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                  ]
+                                ),
+                              )
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    onTap: () {
+                      GoRouter.of(context).push('/summary_of_assignments_in_joined_classroom/${item.name!}/${item.code!}');
                     },
-                    title: Text( joinedClassrooms.classrooms![index].name!),
-                    subtitle: Text(joinedClassrooms.classrooms![index].description!),
-                  ),
-                ),
-              );
-            },
+                  )
+              ],
+            )
           ),
         ),
-      ),
       error: (e, s) =>  Center(child: Text(e.toString()),),
       loading: () => const Center(child: CircularProgressIndicator()),);
   }
